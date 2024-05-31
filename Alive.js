cfa082d112c497fa67d82432e5e964976d437cc2
King Bryant <165713304+Bryant2090@users.noreply.github.com>
2024-05-31 18:01:14 +0000

    Add files via upload

---
+++ Alive.js
@@ -0,0 +1,117 @@
+
+
+/** 
+𝞘𝙏𝞓𝘾𝞖𝞘_𝞛𝘿
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @project_name : ITACHI MD, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @owner: BRYANT TECH
+ * 
+ * 
+ 
+ **/
+
+
+
+
+
+
+
+
+const { bryant } = require('../framework/bryant');
+const {addOrUpdateDataInAlive , getDataFromAlive} = require('../bdd/alive')
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+bryant(
+    {
+        nomCom : 'alive',
+        categorie : 'General'
+        
+    },async (dest,zk,commandeOptions) => {
+
+ const {ms , arg, repondre,superUser} = commandeOptions;
+
+ const data = await getDataFromAlive();
+
+ if (!arg || !arg[0] || arg.join('') === '') {
+
+    if(data) {
+       
+        const {message , lien} = data;
+
+
+        var mode = "public";
+        if ((s.MODE).toLocaleLowerCase() != "yes") {
+            mode = "private";
+        }
+      
+    
+     
+    moment.tz.setDefault('Africa/Nairobi');
+
+// Créer une date et une heure en EAT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+    const alivemsg = `
+*Owner* : ${s.OWNER_NAME}
+*Mode* : ${mode}
+*Date* : ${date}
+*Time* : ${temps}
+
+ ${message}
+ 
+ 
+ *𝛪𝑇𝛥𝐶𝛨𝛪 𝛭𝐷 𝐶𝑅𝛯𝛥𝑇𝛯𝐷 𝐵𝑌 𝐵𝑅𝑌𝛥𝛮𝑇 𝑇𝛯𝐶𝛨 𝛪𝑆 𝛥𝐿𝛪𝛻𝛯*`
+
+ if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Checking for .jpeg or .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(alivemsg);
+    
+}
+
+    } else {
+        if(!superUser) { repondre("there is no alive for this bot") ; return};
+
+      await   repondre(" fool im dead remember You have not yet saved your alive, to do this;  enter after alive your message and your image or video link in this context: .alive message;lien");
+         repondre("don't do fake things :)")
+     }
+ } else {
+
+    if(!superUser) { repondre (" you cant use this command fool Only the owner can  modify the alive") ; return};
+
+  
+    const texte = arg.join(' ').split(';')[0];
+    const tlien = arg.join(' ').split(';')[1]; 
+
+
+    
+await addOrUpdateDataInAlive(texte , tlien)
+
+repondre(' Youre now a sensible person message alive refresh successfully')
+
+}
+    });
